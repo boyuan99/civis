7a4559d8651f9a15a185bcdb33006f031b22a9ae
Bo Yuan <boyuan1021@gmail.com>
2024-05-01 15:58:03 -0500

    Update raster_server.py
    Added position change rate plot and spikes counts

--- servers/raster_server.py
+++ servers/raster_server.py
@@ -1,11 +1,11 @@
-from bokeh.models import ColumnDataSource, TextInput, Button, BoxSelectTool, Spacer
+from bokeh.models import ColumnDataSource, TextInput, Button, BoxSelectTool, Spacer, Div
 from bokeh.plotting import figure
 from bokeh.layouts import column, row
 from bokeh.events import SelectionGeometry, Reset
 import pickle
 import numpy as np
 import json
-from src import StraightMazeTank
+from src import StraightMazeTank, TurnMazeTank
 
 
 def raster_bkapp(doc):
@@ -17,13 +17,14 @@ def raster_bkapp(doc):
         neuron_path = config['ProcessedFilePath'] + session_name + '/' + session_name + '_v7.mat'
         peak_indices_path = config['ProcessedFilePath'] + session_name + "/" + session_name + "_peak_indices.pkl"
 
-        ci = StraightMazeTank(neuron_path)
+        ci = TurnMazeTank(neuron_path)
         print("Successful loaded: " + neuron_path)
 
         with open(peak_indices_path, 'rb') as f:
             peak_indices = pickle.load(f)
 
         spike_times = peak_indices
+        spike_stats = ci.get_spike_statistics(peak_indices)
 
         data = {'x_starts': [], 'y_starts': [], 'x_ends': [], 'y_ends': []}
         for neuron_idx, spikes in enumerate(spike_times):
@@ -33,13 +34,13 @@ def raster_bkapp(doc):
                 data['x_ends'].append(spike_time / ci.ci_rate)
                 data['y_ends'].append(neuron_idx + 1 + 0.7)
 
-        return data, ci
+        return data, ci, spike_stats
 
     # Create initial empty plot
     raster_source = ColumnDataSource({'x_starts': [], 'y_starts': [], 'x_ends': [], 'y_ends': []})
     selected_raster_source = ColumnDataSource({'x_starts': [], 'y_starts': [], 'x_ends': [], 'y_ends': []})
-    line_source = ColumnDataSource({'x': [], 'velocity': [], 'lick': []})
-    selected_line_source = ColumnDataSource({'x': [], 'velocity': [], 'lick': []})
+    line_source = ColumnDataSource({'x': [], 'velocity': [], 'lick': [], 'pstcr': [], 'spike_stats':[]})
+    selected_line_source = ColumnDataSource({'x': [], 'velocity': [], 'lick': [], 'pstcr': [], 'spike_stats':[]})
 
     # Raster plot
     p = figure(width=1000, height=1000, title="Raster Plot", x_axis_label='Time (s)', y_axis_label='Neuron',
@@ -52,11 +53,18 @@ def raster_bkapp(doc):
     # Signal plot
     v = figure(width=1000, height=200, x_range=p.x_range, active_scroll='wheel_zoom',
                tools=['pan', 'wheel_zoom', 'reset'])
-    v.line('x', 'velocity', source=line_source, color='SteelBlue', legend_label='velocity', alpha=0.1)
-    v.line('x', 'lick', source=line_source, color='SandyBrown', legend_label='lick', alpha=0.3)
-    v.line('x', 'velocity', source=selected_line_source, color='SteelBlue', legend_label='velocity', alpha=1)
-    v.line('x', 'lick', source=selected_line_source, color='SandyBrown', legend_label='lick', alpha=1)
+    v.line('x', 'velocity', source=line_source, color='SteelBlue', legend_label='velocity', alpha=1)
+    v.line('x', 'lick', source=line_source, color='SandyBrown', legend_label='lick', alpha=1)
+    v.line('x', 'pstcr', source=line_source, color='Crimson', legend_label='pstcr', alpha=1)
+    v.line('x', 'velocity', source=selected_line_source, color='navy', legend_label='velocity', alpha=1)
+    v.line('x', 'lick', source=selected_line_source, color='gold', legend_label='lick', alpha=1)
+    v.line('x', 'pstcr', source=selected_line_source, color='DarkRed', legend_label='pstcr', alpha=1)
     v.legend.click_policy = 'hide'
+
+    s = figure(width=1000, height=200, x_range=p.x_range, active_scroll='wheel_zoom')
+    s.line('x', 'spike_stats', source=line_source, color='Chocolate', alpha=1, legend_label='spikes count')
+    s.line('x', 'spike_stats', source=selected_line_source, color='Sienna', alpha=1, legend_label='spikes count')
+    s.legend.click_policy = 'hide'
 
     box_select_p = BoxSelectTool(dimensions="width")
     p.add_tools(box_select_p)
@@ -73,13 +81,15 @@ def raster_bkapp(doc):
     def update_data():
         print("Loading Data...")
         session_name = session_input.value
-        data, ci = load_data(session_name)
+        data, ci, spike_stats = load_data(session_name)
         raster_source.data = data
         p.yaxis.ticker = np.arange(1, ci.neuron_num + 1)
         p.yaxis.major_label_overrides = {i + 1: f"Neuron {i + 1}" for i in range(ci.neuron_num)}
         line_source.data = dict(x=ci.t,
                                 velocity=ci.normalize_signal(ci.velocity),
-                                lick=ci.lick)
+                                lick=ci.lick,
+                                pstcr=ci.pstcr,
+                                spike_stats=spike_stats)
 
     load_button.on_click(update_data)
 
@@ -104,18 +114,24 @@ def raster_bkapp(doc):
         filtered_data_x = []
         filtered_data_velocity = []
         filtered_data_lick = []
+        filtered_data_pstcr = []
+        filtered_data_spikes = []
 
         for i, x_val in enumerate(line_source.data['x']):
             if x0 <= x_val <= x1:
                 filtered_data_x.append(x_val)
                 filtered_data_velocity.append(line_source.data['velocity'][i])
                 filtered_data_lick.append(line_source.data['lick'][i])
+                filtered_data_pstcr.append(line_source.data['pstcr'][i])
+                filtered_data_spikes.append(line_source.data['spike_stats'][i])
 
         # Update the line_source with the filtered data
         selected_line_source.data = {
             'x': filtered_data_x,
             'velocity': filtered_data_velocity,
             'lick': filtered_data_lick,
+            'pstcr': filtered_data_pstcr,
+            'spike_stats': filtered_data_spikes,
         }
 
     p.on_event(SelectionGeometry, selection_handler)
@@ -123,12 +139,12 @@ def raster_bkapp(doc):
 
     def clear_selected_sources(event):
         selected_raster_source.data = {'x_starts': [], 'y_starts': [], 'x_ends': [], 'y_ends': []}
-        selected_line_source.data = {'x': [], 'velocity': [], 'lick': []}
+        selected_line_source.data = {'x': [], 'velocity': [], 'lick': [], 'pstcr': [], 'spike_stats':[]}
 
     p.on_event(Reset, clear_selected_sources)
     v.on_event(Reset, clear_selected_sources)
 
     # Layout
     blank_left = Spacer(width=30)
-    layout = row(blank_left, column(row(session_input, load_button), p, v))
+    layout = row(blank_left, column(row(session_input, load_button), p, v, s))
     doc.add_root(layout)
